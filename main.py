019629fc7a2fa15d2a23d83e480e1ae08105f6ec
Hector Carbajal <hector27c7@gmail.com>
2026-04-14 13:15:29 -0700

    added database connection

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.db.session import engine, Base
 
 app = FastAPI(title="HabitXP API")
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
